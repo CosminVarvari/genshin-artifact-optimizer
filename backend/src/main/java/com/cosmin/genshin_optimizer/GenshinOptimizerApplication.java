ba46c5ccce8fa27fa9956c52c1cd8a8311b62bd6
Cosmin Varvari <cosmin.varvari@yahoo.com>
2025-06-03 15:30:42 +0300

    Initial project structure with Spring Boot backend

---
+++ backend/src/main/java/com/cosmin/genshin_optimizer/GenshinOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.cosmin.genshin_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinOptimizerApplication.class, args);
+	}
+
+}
